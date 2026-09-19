2e105ca7895354b087c4f8f38449f8d2a01d75df
Morpa <andremorpa@gmail.com>
2026-09-19 08:50:47 +0100

    feat(services): add login

---
+++ lib/data/services/api/api_client.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_mvvm/data/services/api/models/login_request.dart';
+import 'package:flutter_mvvm/data/services/api/models/login_response.dart';
+import 'package:flutter_mvvm/utils/result.dart';
+
+class ApiClient {
+  final String _apiURL;
+  final Dio _dio;
+
+  const ApiClient({required this._apiURL, required this._dio});
+
+  Future<Result<LoginResponse>> login(LoginRequest loginRequest) async {
+    try {
+      final endpoint = '$_apiURL/auth/login';
+
+      final response = await _dio.post(endpoint, data: loginRequest.toJson());
+
+      if (response.statusCode == 200) {
+        final result = LoginResponse.fromJson(response.data);
+        return Result.ok(result);
+      }
+      return Result.error(Exception('Ocorreu um erro ao realizar o login'));
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
+}
